54df498289bcbe862764dbe41bb66e16591cc217
younwony <wony9324@naver.com>
2025-06-12 22:15:10 +0900

    feat : 1장

---
+++ src/study/Person.kt
@@ -0,0 +1,5 @@
+package study
+
+class Person(name: String) {
+    var name = "";
+}
--- src/study/main.kt
+++ src/study/main.kt
@@ -5,4 +5,8 @@ fun main() {
     val number1 = 10L; // 변경 불가능한 변수, Java final
 
     // tip : 우선 val 변수로 만들고 필요한 경우 var로 변경
+    // Java의 Long과 long의 차이점은 Kotlin에서는 없습니다.
+
+    var number3: Long? = null // null을 허용하는 변수, 변수에 ?를 추가해야 null을 허용합니다.
+    var person = Person("윤원희") // Java의 new 키워드 없이 객체 생성
 }
